fef96429f3f76d4a31e1a006062b4fab2ac34387
LauroRigon <laurorigon@gmail.com>
2017-04-16 18:46:07 -0300

    first commit
    migrations and models done!

---
+++ database/migrations/2017_04_16_205525_create_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('content');
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('avatar')->nullable();
+            $table->boolean('allowed')->default(0);
+            $table->integer('view_count')->default(0);
+            $table->timestamp('published_at')->nullable();
+
+            //referencias
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
