aa5317b62edf17c294d196a865dfc8312345e197
LauroRigon <laurorigon@gmail.com>
2017-08-10 14:41:33 -0300

    Register/pass recovery... routes deleted

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-Auth::routes();
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 /*
  * Rotas da área de admin do site
