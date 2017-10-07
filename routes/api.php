2848e0d0eaf300b002577db2a65933bf933e116e
LauroRigon <laurorigon@gmail.com>
2017-10-07 19:51:25 -0300

    Api routes

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,24 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Polls
 Route::get('/polls/getPolls', 'Api\PollController@getPolls');
 Route::get('/polls/getPoll/{poll}', 'Api\PollController@getPoll');
 Route::post('/polls/addVote', 'Api\PollController@addVote');
+
+//Supporters
+Route::get('/supporters/getSupporters/{side}', 'Api\SupportersController@getSupporters');
+
+//Posts
+Route::get('/posts/getPosts', 'Api\PostController@getPosts');
+Route::get('/posts/{slug}', 'Api\PostController@getPost');
+
+//Programming
+Route::get('/programming/today', 'Api\ProgrammingController@getTodayProgramming');
+Route::get('/programming', 'Api\ProgrammingController@getProgramming');
+
+//Speakers
+Route::get('/speakers', 'Api\UserController@getUsers');
+
+//MusicOrders
+Route::post('/musicOrder/store', 'Api\MusicOrderController@store');
