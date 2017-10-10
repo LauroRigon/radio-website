2609f65a3b3b879e826437190474267d7dcaca7e
LauroRigon <laurorigon@gmail.com>
2017-10-10 10:19:44 -0300

    api controller methods

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use App\Post;
@@ -28,5 +29,27 @@ class PostController extends Controller
         $post = Post::where('slug', $slug)->where('is_about', 0)->with('galleries')->with('user')->get()[0];
 
         return response()->json($post, 200);
+    }
+
+    /**
+     * Retorna todas postagens pela categoria (para o front)
+     * @return \Illuminate\Http\Response
+     */
+    public function getByCategory(Request $request, $categoria) {
+        $category = Category::where('name', $categoria)->first();
+
+        $posts = Post::where('category_id', $category->id)->where('allowed', 1)->where('is_about', 0)->orderBy('published_at', 'desc')->with('user')->paginate(10);
+
+        return response()->json($posts, 200);
+    }
+
+    /**
+     * Retorna a postagem de 'sobre' (para o front)
+     * @return \Illuminate\Http\Response
+     */
+    public function getAbout(Request $request) {
+        $about = Post::where('is_about', 1)->with('galleries')->first();
+
+        return response()->json($about, 200);
     }
 }
