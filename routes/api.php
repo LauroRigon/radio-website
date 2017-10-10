c971380f2e4a76a09bb6ccc7d674068de31bec53
LauroRigon <laurorigon@gmail.com>
2017-10-10 10:18:28 -0300

    api routes update

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,10 @@ Route::post('/polls/addVote', 'Api\PollController@addVote');
 Route::get('/supporters/getSupporters/{side}', 'Api\SupportersController@getSupporters');
 
 //Posts
+Route::get('/posts/getAbout', 'Api\PostController@getAbout'); //About
 Route::get('/posts/getPosts', 'Api\PostController@getPosts');
 Route::get('/posts/{slug}', 'Api\PostController@getPost');
+Route::get('/posts/byCategory/{category}', 'Api\PostController@getByCategory');
 
 //Programming
 Route::get('/programming/today', 'Api\ProgrammingController@getTodayProgramming');
@@ -32,3 +34,6 @@ Route::get('/speakers', 'Api\UserController@getUsers');
 
 //MusicOrders
 Route::post('/musicOrder/store', 'Api\MusicOrderController@store');
+
+//Categories
+Route::get('/categories', 'Api\CategoryController@getCategories');
